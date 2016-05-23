9643acb3e68c1b45ca76d914e6465e7889554c40
nieka <niekhoffmans@live.nl>
2016-05-23 12:15:12 +0200

    input reader

--- src/com/niekenerik/InputReader.java
+++ src/com/niekenerik/InputReader.java
@@ -1,9 +1,26 @@
 package com.niekenerik;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class InputReader {
 
+    private BufferedReader bufferedReader;
+
+    public InputReader(){
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
     public String readInput(){
-        return "";
+        String input = "";
+        try {
+            input =  bufferedReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return input;
     }
 
 }
